94c061ac8965506cd22767d2088a660e9c8d2c2c
igatto <grzegorzsend@gmail.com>
2014-12-13 11:12:33 +0100

    Added games view

---
+++ app/controllers/dashboard/games_controller.rb
@@ -0,0 +1,11 @@
+module Dashboard
+  class GamesController < ApplicationController
+    expose(:game)
+
+    def index
+    end
+
+    def new
+    end
+  end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   enum disk_condition: { poor: 0, good: 1, mint: 2 }
-  enum box_condition: {poor: 0, good: 1, mind: 2}
+  enum box_condition: { brand_new: 0, used: 1 }
 
   validates :title, :description, :disk_condition, :box_condition, presence: true
 end
