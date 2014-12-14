2f029d7eb38e82456fe73896bd24e5396a6a8427
igatto <grzegorzsend@gmail.com>
2014-12-14 16:04:30 +0100

    Search conditions

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,4 +2,8 @@ module DashboardHelper
   def generate_collection(name)
     Game.public_send(name).collect { |name, _| [name.humanize, name]  }
   end
+
+  def generate_collection_int(name)
+    Game.public_send(name).collect { |name, id| [name.humanize, id]  }
+  end
 end
--- app/searches/game_search.rb
+++ app/searches/game_search.rb
@@ -1,7 +1,7 @@
 class GameSearch < Searchlight::Search
   search_on Game.all
 
-  searches :title, :category_id
+  searches :title, :category_id, :disk_condition, :box_condition
 
   def search_title
     search.where('title ILIKE ?', "%#{title}%")
@@ -9,5 +9,13 @@ class GameSearch < Searchlight::Search
 
   def search_category_id
     search.joins(:categories).where(categories: { id: category_id })
+  end
+
+  def search_disk_condition
+    search.where(disk_condition: disk_condition)
+  end
+
+  def box_condition
+    search.where(box_condition: box_condition.map(&:i))
   end
 end
