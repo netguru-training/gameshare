902f65824ed5c4ed03a4409d9e1038cc66c5bf49
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-12-14 15:11:33 +0100

    Customiza game policy

--- app/policies/game_policy.rb
+++ app/policies/game_policy.rb
@@ -1,9 +1,27 @@
 class GamePolicy < ApplicationPolicy
   def add_to_wishlist?
-    true
+    not has_in_collection?
   end
 
   def add_to_collection?
-    true
+    not has_in_wishlist?
+  end
+
+  def remove_from_collection?
+    has_in_collection?
+  end
+
+  def remove_from_wishlist?
+    has_in_wishlist?
+  end
+
+  private
+
+  def has_in_wishlist?
+    WishlistItem.avaliable(user, record)
+  end
+
+  def has_in_collection?
+    GameCollectionItem.avaliable(user, record)
   end
 end
